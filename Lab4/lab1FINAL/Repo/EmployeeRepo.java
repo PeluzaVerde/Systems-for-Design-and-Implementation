e97969caf660d5c95f6ccd584b519012dc1a7cbc
PeluzaVerde <91471457+PeluzaVerde@users.noreply.github.com>
2023-03-26 21:16:25 +0300

    g

--- Lab4/lab1FINAL/Repo/EmployeeRepo.java
+++
@@ -1,7 +0,0 @@
-package group.lab1FINAL.Repo;
-
-import group.lab1FINAL.Model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepo extends JpaRepository<Employee,Long>{
-}
--- Lab4/lab1FINAL/Repo/RestaurantEmployeeRepo.java
+++
@@ -1,7 +0,0 @@
-package group.lab1FINAL.Repo;
-import group.lab1FINAL.Model.RestaurantEmployee;
-import group.lab1FINAL.Model.Review;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-public interface RestaurantEmployeeRepo extends JpaRepository<RestaurantEmployee,Long>{
-}
