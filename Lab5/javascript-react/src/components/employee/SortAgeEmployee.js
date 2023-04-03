b3ac7d110f0f0d6d393f12a8ed95ecc707bf4758
PeluzaVerde <91471457+PeluzaVerde@users.noreply.github.com>
2023-04-03 11:11:53 +0300

    sort

--- Lab5/javascript-react/src/components/employee/SortAgeEmployee.js
+++ Lab5/javascript-react/src/components/employee/SortAgeEmployee.js
@@ -30,6 +30,7 @@ export default function SortAgeEmployee() {
         setEmployees(result.data);
         //  SortEmployee();
         employees.sort( compare );
+        setEmployees(employees);
         
 
     }
