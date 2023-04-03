276d79baa04fe004f7191f7ae8de92943b08d24b
PeluzaVerde <91471457+PeluzaVerde@users.noreply.github.com>
2023-04-03 09:23:38 +0300

    changes

--- Lab5/lab1FINAL/src/main/java/group/lab1FINAL/Controller/EmployeeControllerJson.java
+++ Lab5/lab1FINAL/src/main/java/group/lab1FINAL/Controller/EmployeeControllerJson.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://illustrious-cactus-75f2eb.netlify.app/")
 @RestController
 public class EmployeeControllerJson {
     private EmployeeServiceImpl employeeService;
